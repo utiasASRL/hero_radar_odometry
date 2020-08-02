f9e41fc3e679aca62600a57e95632a0ccc698192
dyoon823 <david.yoon@robotics.utias.utoronto.ca>
2020-08-02 15:22:56 -0400

    added early stopping to trainer, with loading and saving of checkpoints deleted trainer/dummy_loader

--- utils/trainer.py
+++ utils/trainer.py
@@ -1,3 +1,4 @@
+import os
 import torch
 import numpy as np
 
@@ -22,8 +23,13 @@ class Trainer():
 
         self.start_epoch = 0
         self.min_val_loss = np.Inf
-        # TODO: load network parameters if resuming
-        # TODO: load optimizer states if resuming
+        self.result_path = os.path.join('results', config['session_name'])
+        self.checkpoint_path = os.path.join(self.result_path, 'chkp.tar')
+
+        # load network parameters and optimizer if resuming from previous session
+        if config['previous_session'] != "":
+            resume_path = "{}/{}/{}".format('results', config['previous_session'], 'chkp.tar')
+            self.resume_checkpoint(resume_path)
 
         # data loaders
         self.train_loader = train_loader
@@ -46,21 +52,67 @@ class Trainer():
                 self.optimizer.zero_grad()
 
                 # TODO: forward prop
+                loss = 0
 
                 # TODO: backwards prop
+
+        return loss
+
+    def valid_epoch(self, epoch):
+        self.model.eval()
+
+        # TODO: add anomoly detection
+        loss = 0
+        with torch.no_grad():
+            for i_batch, batch_sample in enumerate(self.train_loader):
+                self.optimizer.zero_grad()
+
+                # TODO: forward prop
+                loss += 0    # summation!
+
+
+        return loss
 
     def train(self):
         """
         Full training loop
         """
-        # TODO: validation and early stopping
-        # early_stopping = EarlyStopping(patience=self.config['trainer']['patience'],
-        #                                best_score=-self.min_val_loss,
-        #                                val_loss_min=self.min_val_loss)
+        # validation and early stopping
+        if self.config['validate']['on']:
+            train_loss = early_stopping = EarlyStopping(patience=self.config['trainer']['validate']['patience'],
+                                                        val_loss_min=self.min_val_loss)
 
         for epoch in range(self.start_epoch, self.config['trainer']['max_epochs']):
 
             self.train_epoch(epoch)
-            # val_loss = self.valid_epoch(epoch, self.valid_data_loader,
-            #                             self.valid_stats, 'validation')
+
+            if self.config['validate']['on']:
+                # check for validation set and early stopping
+                val_loss = self.valid_epoch(epoch)
+                stop_flag, loss_decrease_flag, self.min_val_loss = early_stopping.check_stop(
+                    val_loss, self.model, self.optimizer, self.checkpoint_path, epoch)
+
+                if stop_flag:
+                    break
+            else:
+                # save out every epoch if no validation
+                torch.save({'epoch': epoch,
+                            'model_state_dict': self.model.state_dict(),
+                            'optimizer_state_dict': self.optimizer.state_dict(),
+                            'loss': val_loss,
+                            }, self.checkpoint_path)
+
+    def resume_checkpoint(self, checkpoint_path):
+        """
+        Resume from saved checkpoint
+        :param checkpoint_path: Modol filename and path to be resumed
+        """
+
+        checkpoint = torch.load(checkpoint_path)
+        self.model.load_state_dict(checkpoint['model_state_dict'])
+        self.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        self.start_epoch = checkpoint['epoch'] + 1
+        self.min_val_loss = checkpoint['loss']
+
+        print("Resume training from epoch {}".format(self.start_epoch))
 
