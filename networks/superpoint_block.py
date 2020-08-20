d21e7c6eb08fc5253371658e10a2561577412c76
dyoon823 <david.yoon@robotics.utias.utoronto.ca>
2020-08-20 14:08:51 -0400

    changed superpoint back to smaller network (better).

--- networks/superpoint_block.py
+++ networks/superpoint_block.py
@@ -28,39 +28,39 @@ class SuperpointBlock(nn.Module):
         self.n_weight = config['networks']['unet']['n_weight_score']
 
         # encoder
-        self.inc = DoubleConv(self.n_channels, 64)    # 512 x 384 (out size after layer)
-        self.inc2 = DoubleConv(64, 64)
-        self.down1 = Down(64, 128)                # B x 64 x 256 x 192
-        self.down1_2 = DoubleConv(128, 128)                # B x 64 x 256 x 192
-        self.down2 = Down(128, 256)               # B x 128 x 128 x 96
-        self.down2_2 = DoubleConv(256, 256)               # B x 128 x 128 x 96
-        self.down3 = Down(256, 512)              # B x 256 x 64 x 48 (H/8 x W/8)
-
-        # decoders
-        self.decode_detector = DoubleConv(512, 512)
-        self.out_detector = OutConv(512, 64)
-
-        self.decode_weight = DoubleConv(512, 512)
-        self.out_weight = OutConv(512, 64*self.n_weight)
-
-        self.decode_desc = DoubleConv(512, 512)
-        self.out_desc = OutConv(512, 256)
-
         # self.inc = DoubleConv(self.n_channels, 64)    # 512 x 384 (out size after layer)
         # self.inc2 = DoubleConv(64, 64)
         # self.down1 = Down(64, 128)                # B x 64 x 256 x 192
+        # self.down1_2 = DoubleConv(128, 128)                # B x 64 x 256 x 192
         # self.down2 = Down(128, 256)               # B x 128 x 128 x 96
+        # self.down2_2 = DoubleConv(256, 256)               # B x 128 x 128 x 96
         # self.down3 = Down(256, 512)              # B x 256 x 64 x 48 (H/8 x W/8)
         #
         # # decoders
-        # self.decode_detector = SingleConv(512, 512)
+        # self.decode_detector = DoubleConv(512, 512)
         # self.out_detector = OutConv(512, 64)
         #
-        # self.decode_weight = SingleConv(512, 512)
+        # self.decode_weight = DoubleConv(512, 512)
         # self.out_weight = OutConv(512, 64*self.n_weight)
         #
-        # self.decode_desc = SingleConv(512, 512)
+        # self.decode_desc = DoubleConv(512, 512)
         # self.out_desc = OutConv(512, 256)
+
+        self.inc = DoubleConv(self.n_channels, 64)    # 512 x 384 (out size after layer)
+        self.inc2 = DoubleConv(64, 64)
+        self.down1 = Down(64, 128)                # B x 64 x 256 x 192
+        self.down2 = Down(128, 256)               # B x 128 x 128 x 96
+        self.down3 = Down(256, 512)              # B x 256 x 64 x 48 (H/8 x W/8)
+
+        # decoders
+        self.decode_detector = SingleConv(512, 512)
+        self.out_detector = OutConv(512, 64)
+
+        self.decode_weight = SingleConv(512, 512)
+        self.out_weight = OutConv(512, 64*self.n_weight)
+
+        self.decode_desc = SingleConv(512, 512)
+        self.out_desc = OutConv(512, 256)
 
     def forward(self, x):
         batch_size, _, height, width = x.size()
@@ -68,9 +68,9 @@ class SuperpointBlock(nn.Module):
         x = self.inc(x)
         x = self.inc2(x)
         x = self.down1(x)
-        x = self.down1_2(x)
+        # x = self.down1_2(x)
         x = self.down2(x)
-        x = self.down2_2(x)
+        # x = self.down2_2(x)
         x = self.down3(x)                           # B x 256 x H/8 x W/8
 
         detector = self.decode_detector(x)                # B x 65 x H/8 x W/8
--- precompute_kitti_images.py
+++ precompute_kitti_images.py
@@ -13,35 +13,24 @@ import json
 from utils.helper_func import *
 
 parser = argparse.ArgumentParser(description='Point Cloud Registration')
-parser.add_argument('--config', default=None, type=str,
+parser.add_argument('--data_dir', default=None, type=str,
                     help='config file path (default: None)')
 parser.add_argument('--save_dir', type=str, default='.', metavar='N',
                     help='Save to current directory by default')
-parser.add_argument('--sequence_number', type=str, default='00', metavar='N',
-                    help="Sequence number. String, e.g '00'")
+# parser.add_argument('--sequence_number', type=str, default='00', metavar='N',
+#                     help="Sequence number. String, e.g '00'")
 # parser.add_argument('--num_frames', type=int, default=10, metavar='N',
 #                     help='Number of frames to write. Integer value: e.g 10. Set to -1 for all frames.')
 
 args = parser.parse_args()
 
-with open(args.config) as f:
-    config = json.load(f)
-
-save_dir = os.path.join(args.save_dir, args.sequence_number)
-dataset = pykitti.odometry(config["dataset"]["data_dir"], args.sequence_number)
-
-# take point cloud
-num_poses = len(dataset.poses)
-sample_n = num_poses
-sample_idx = np.arange(sample_n)
-
-# load image configs
-azi_res = config["dataset"]["images"]["azi_res"]
-azi_min = config["dataset"]["images"]["azi_min"]
-azi_max = config["dataset"]["images"]["azi_max"]
-ele_res = config["dataset"]["images"]["ele_res"]
-ele_min = config["dataset"]["images"]["ele_min"]
-ele_max = config["dataset"]["images"]["ele_max"]
+# set image configs
+azi_res = 0.5
+azi_min = -180.0
+azi_max = 180.0
+ele_res = 0.5   # ele parameters don't matter if mapping laser to row
+ele_min = -25.0
+ele_max = 3.0
 input_channel = ['vertex', 'intensity', 'range'] # fix input to all channels
 
 # image sizes
@@ -49,24 +38,34 @@ horizontal_pix = np.int32((azi_max - azi_min) / azi_res)
 vertical_pix = np.int32((ele_max - ele_min) / ele_res)
 geometry_img = np.zeros((3, vertical_pix, horizontal_pix), dtype=np.float32)
 
-# inspect vertex map
-for sample_i in sample_idx:
-    sample_velo = dataset.get_velo(sample_i)
+# set sequences
+seq_list = ["00","01","02","03","04","05","06","07","08","09","10"]
+for seq_num in seq_list:
+    # file directories
+    save_dir = os.path.join(args.save_dir, seq_num)
+    dataset = pykitti.odometry(args.data_dir, seq_num)
 
-    # if config["dataset"]["images"]["map_laser_to_row"]:
-    geometry_img, input_img = pc2img_laser_to_row(sample_velo, azi_res, azi_min, azi_max,
-                                                  input_channel, horizontal_pix)
-    # else:
-    #     geometry_img, input_img = pc2img(sample_velo, geometry_img, azi_res, azi_min, azi_max,
-    #                                     ele_res, ele_min, ele_max, input_channel,
-    #                                     horizontal_pix, vertical_pix)
+    # take point cloud
+    num_poses = len(dataset.poses)
+    sample_n = num_poses
+    sample_idx = np.arange(sample_n)
+    for sample_i in sample_idx:
+        sample_velo = dataset.get_velo(sample_i)
 
-    if not os.path.exists(save_dir):
-        os.makedirs(save_dir)
+        # if config["dataset"]["images"]["map_laser_to_row"]:
+        geometry_img, input_img = pc2img_laser_to_row(sample_velo, azi_res, azi_min, azi_max,
+                                                      input_channel, horizontal_pix)
+        # else:
+        #     geometry_img, input_img = pc2img(sample_velo, geometry_img, azi_res, azi_min, azi_max,
+        #                                     ele_res, ele_min, ele_max, input_channel,
+        #                                     horizontal_pix, vertical_pix)
 
-    # plt.imsave('{}/intensity_{}.png'.format(save_dir, sample_i), input_img)
-    np.save('{}/{}.npy'.format(save_dir, sample_i), input_img)
-    print(sample_i, sample_n)
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
+
+        # plt.imsave('{}/intensity_{}.png'.format(save_dir, sample_i), input_img)
+        np.save('{}/{}.npy'.format(save_dir, sample_i), input_img)
+        print(sample_i, sample_n)
 
 
 print("====================")
