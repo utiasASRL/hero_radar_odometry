a4f9e8e80e7e2537de9f5f3fd9833cfd8a6fb221
dyoon823 <david.yoon@robotics.utias.utoronto.ca>
2020-12-16 11:34:40 -0500

    added unit test for solver class implementation of steam

--- test/test_steam.py
+++ test/test_steam.py
@@ -3,6 +3,7 @@ import random
 import numpy as np
 import torch
 import cpp.build.steampy as steampy
+import cpp.build.SteamSolver as SteamCpp
 
 class TestSteam(unittest.TestCase):
     def test_basic(self):
@@ -37,6 +38,49 @@ class TestSteam(unittest.TestCase):
         # 2nd pose will be T_21
         R = torch.from_numpy(poses[1, 0, :3, :3])
         t = torch.from_numpy(poses[1, 0, :3, 3:])
+
+        R_err = torch.sum(R - R_gt)
+        t_err = torch.sum(t - t_gt)
+        self.assertTrue(R_err < 1e-4)
+        self.assertTrue(t_err < 1e-4)
+
+    def test_solver_class(self):
+        N = 100
+        D = 2
+        src = torch.randn(D, N)
+        theta = np.pi / 8
+        R_gt = torch.eye(2)
+        R_gt[:2, :2] = torch.tensor([[np.cos(theta), np.sin(theta)], [-np.sin(theta), np.cos(theta)]])
+        t_gt = torch.tensor([[1], [2]])
+        out = R_gt@src + t_gt
+        zeros_vec = np.zeros((N, 1), dtype=np.float32)
+
+        # points must be list of N x 3
+        points2 = out.T.detach().cpu().numpy()
+        points1 = src.T.detach().cpu().numpy()
+
+        # weights must be list of N x 3 x 3
+        identity_weights = np.tile(np.expand_dims(np.eye(3, dtype=np.float32), 0), (N, 1, 1))
+
+        # poses are window_size x 4 x 4
+        window_size = 2
+        poses = np.tile(
+            np.expand_dims(np.eye(4, dtype=np.float32), 0),
+            (window_size, 1, 1))
+
+        # run steam
+        dt = 0.25
+        solver = SteamCpp.SteamSolver(dt, window_size)
+        solver.setMeas([np.concatenate((points2, zeros_vec), 1)],
+                       [np.concatenate((points1, zeros_vec), 1)], [identity_weights])
+        solver.optimize()
+
+        # get pose output
+        solver.getPoses(poses)
+
+        # 2nd pose will be T_21
+        R = torch.from_numpy(poses[1, :2, :2])
+        t = torch.from_numpy(poses[1, :2, 3:])
 
         R_err = torch.sum(R - R_gt)
         t_err = torch.sum(t - t_gt)
