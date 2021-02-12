37e5d6e128d9d391f0a7feae262b9a4608d5854b
keenan-burnett <keenburn2004@gmail.com>
2021-02-12 16:31:07 -0500

    Fixed bug with saving pdf files. download_results uses wget now instead of gdown.

--- download_results.sh
+++ download_results.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-pip install gdown
-gdown https://drive.google.com/uc?id=1FMfpuNACCcT1CzhTixYvclLGSeGMYJyo -O icra_odom.zip
-apt install unzip
+wget https://drive.google.com/uc?id=1FMfpuNACCcT1CzhTixYvclLGSeGMYJyo -o icra_odom.zip
 unzip icra_odom.zip
 mkdir -p results/icra21
 mv accuracy*.csv results/icra21
