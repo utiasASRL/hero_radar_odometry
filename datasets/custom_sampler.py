ca14bd3db9d02c2f805cef7c5c940e19a10bd35b
keenan-burnett <keenburn2004@gmail.com>
2021-02-20 20:58:27 -0500

    Revert "Added code for upsampling frames with high velocity or high angular velocity."
    This reverts commit 5ebcbc94294e1256fe771cfb3c0069976f34362a.

--- datasets/custom_sampler.py
+++ datasets/custom_sampler.py
@@ -2,19 +2,12 @@ from itertools import accumulate
 from torch.utils.data import Sampler, SubsetRandomSampler
 
 class RandomWindowBatchSampler(Sampler):
-    def __init__(self, batch_size, window_size, seq_lens, upsamplerates=[], drop_last=True):
+    def __init__(self, batch_size, window_size, seq_lens, drop_last=True):
         valid_frames = []
         seq_len_cumsum = list(accumulate([0] + seq_lens))
         for j, len_j in enumerate(seq_len_cumsum[1:]):
             valid_frames += list(range(seq_len_cumsum[j], len_j - window_size + 1))
-        valid_frames2 = valid_frames.copy()
-        if len(upsamplerates) > 0:
-            for frame in valid_frames:
-                if upsamplerates[frame] > 0:
-                    ups = int(upsamplerates[frame])
-                    for j in range(ups):
-                        valid_frames2.append(frame)
-        self.sampler = SubsetRandomSampler(valid_frames2)
+        self.sampler = SubsetRandomSampler(valid_frames)
         self.batch_size = batch_size
         self.window_size = window_size
         self.drop_last = drop_last
