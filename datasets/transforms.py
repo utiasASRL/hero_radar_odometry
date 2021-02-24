848d558e8e33d8d5fd2bf3c5968d99edc4998a27
dyoon823 <david.yoon@robotics.utias.utoronto.ca>
2021-02-24 11:54:44 -0500

    Removed azimuth flip. Also updated augmentation scheme for radar and mask.

--- datasets/transforms.py
+++ datasets/transforms.py
@@ -11,42 +11,62 @@ def augmentBatch(batch, config):
     data = batch['data'].numpy()    # this seems to return a reference, not a copy
     # data2 = batch['data'].numpy().copy()    # this seems to return a reference, not a copy
     mask = batch['mask'].numpy()
+    # mask2 = batch['mask'].numpy().copy()
     times_img = batch['times_img'].numpy()
     # times_img2 = batch['times_img'].numpy().copy()
     azimuths = batch['azimuths'].numpy()
     azimuth_times = batch['azimuth_times'].numpy()
+    polar = batch['polar'].numpy()
+    polar_mask = batch['polar_mask'].numpy()
+
     B, C, H, W = data.shape
     T_aug = []
     for i in range(B):
         if np.mod(i, config['window_size']) == 0:
             continue
-        img = data[i].squeeze()
-        mmg = mask[i].squeeze()
+        # img = data[i].squeeze()
+        # mmg = mask[i].squeeze()
         # tmg = times_img[i].squeeze()
         rot = np.random.uniform(-rot_max, rot_max)
-        M = cv2.getRotationMatrix2D((W / 2, H / 2), rot * 180 / np.pi, 1.0)
-        data[i] = cv2.warpAffine(img, M, (W, H), flags=cv2.INTER_CUBIC).reshape(C, H, W)
-        mask[i] = cv2.warpAffine(mmg, M, (W, H), flags=cv2.INTER_CUBIC).reshape(C, H, W)
-
-        azimuth_step = azimuths[i, 1] - azimuths[i, 0]
-        index_step = int(np.fabs(rot) // azimuth_step)
-        azimuth_times[i] = np.roll(azimuth_times[i], -int(np.sign(rot))*index_step, axis=0)
+        # M = cv2.getRotationMatrix2D((W / 2, H / 2), rot * 180 / np.pi, 1.0)
+        # data2[i] = cv2.warpAffine(img, M, (W, H), flags=cv2.INTER_CUBIC).reshape(C, H, W)
+        # mask2[i] = cv2.warpAffine(mmg, M, (W, H), flags=cv2.INTER_CUBIC).reshape(C, H, W)
         # times_img[i] = cv2.warpAffine(tmg, M, (W, H), flags=cv2.INTER_CUBIC).reshape(C, H, W)
         # azimuths[i] - rot
+
+        azimuth_step = float(azimuths[i, 1] - azimuths[i, 0])
+        index_steps = int(np.fabs(rot) // azimuth_step)
+        azimuth_times[i] = np.roll(azimuth_times[i], -int(np.sign(rot))*index_steps, axis=0)
         times_img[i] = radar_polar_to_cartesian(azimuths[i], azimuth_times[i],
                                              config['radar_resolution'],
                                              config['cart_resolution'],
                                              config['cart_pixel_width']).astype(np.float32)
-        # fig, ax = plt.subplots(2, 2)
+
+        polar[i] = np.roll(polar[i], -int(np.sign(rot))*index_steps, axis=0)
+        data[i] = radar_polar_to_cartesian(azimuths[i], polar[i],
+                                             config['radar_resolution'],
+                                             config['cart_resolution'],
+                                             config['cart_pixel_width']).astype(np.float32)
+
+        polar_mask[i] = np.roll(polar_mask[i], -int(np.sign(rot))*index_steps, axis=0)
+        mask[i] = radar_polar_to_cartesian(azimuths[i], polar_mask[i],
+                                             config['radar_resolution'],
+                                             config['cart_resolution'],
+                                             config['cart_pixel_width']).astype(np.float32)
+
+        # fig, ax = plt.subplots(3, 2)
         # im1 = ax[0, 0].imshow(times_img[i, 0])
         # im2 = ax[0, 1].imshow(times_img2[i, 0])
         # ax[1, 0].imshow(data[i, 0])
         # ax[1, 1].imshow(data2[i, 0])
+        # ax[2, 0].imshow(mask[i, 0])
+        # ax[2, 1].imshow(mask2[i, 0])
         # fig.colorbar(im1, ax=ax[0, 0])
         # fig.colorbar(im2, ax=ax[0, 1])
         # plt.show()
 
-        T_aug += [torch.from_numpy(get_transform(0, 0, -rot))]
+        # T_aug += [torch.from_numpy(get_transform(0, 0, -rot))]
+        T_aug += [torch.from_numpy(get_transform(0, 0, azimuth_step*index_steps))]
     batch['data'] = torch.from_numpy(data)
     batch['times_img'] = torch.from_numpy(times_img)
     batch['mask'] = torch.from_numpy(mask > 0.5).type(batch['data'].dtype)    # make into a binary mask
