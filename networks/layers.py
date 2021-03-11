09ffa32a589b4687c5f27c7a785f2abd89dc9662
keenan-burnett <keenburn2004@gmail.com>
2021-03-11 23:05:15 +0000

    Fixed a bug in determining the depth for transposed convolution.

--- networks/layers.py
+++ networks/layers.py
@@ -2,6 +2,7 @@
     Parts of the U-Net model
     Code from: https://github.com/milesial/Pytorch-UNet/blob/master/unet/unet_parts.py
 """
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -42,26 +43,21 @@ class Up(nn.Module):
 
     def __init__(self, in_channels, out_channels, bilinear=True):
         super().__init__()
-
         # if bilinear, use the normal convolutions to reduce the number of channels
         if bilinear:
             self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
         else:
-            self.up = nn.ConvTranspose2d(in_channels, in_channels, kernel_size=2, stride=2)
-
+            d = int(pow(2, np.floor(np.log(in_channels) / np.log(2))))
+            self.up = nn.ConvTranspose2d(d, d, kernel_size=2, stride=2)
         self.conv = DoubleConv(in_channels, out_channels)
 
     def forward(self, x1, x2):
         x1 = self.up(x1)
-
-        # input is CHW
+        # input is NCHW
         diffY = torch.tensor([x2.size()[2] - x1.size()[2]])
         diffX = torch.tensor([x2.size()[3] - x1.size()[3]])
-
         x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2, diffY // 2, diffY - diffY // 2])
-
         x = torch.cat([x2, x1], dim=1)
-
         return self.conv(x)
 
 class OutConv(nn.Module):
