Fixed more bugs in the mcransac code. It reduces the rotation error during evaluation but translation increases by a little bit.

networks/hero.py
@@ -53,7 +53,7 @@ class HERO(torch.nn.Module):
         time_tgt = torch.index_select(timestamps, 0, tgt_ids)
         time_src = torch.index_select(timestamps, 0, src_ids)
         R_tgt_src_pred, t_tgt_src_pred = self.solver.optimize(keypoint_coords_xy, pseudo_coords_xy, match_weights,
-                                                              keypoint_ints, timt_tgt, time_src)
+                                                              keypoint_ints, time_tgt, time_src)
 
         return {'R': R_tgt_src_pred, 't': t_tgt_src_pred, 'scores': weight_scores, 'tgt': keypoint_coords_xy,
                 'src': pseudo_coords_xy, 'match_weights': match_weights, 'keypoint_ints': keypoint_ints,

networks/softmax_ref_matcher.py
@@ -32,11 +32,11 @@ class SoftmaxRefMatcher(nn.Module):
         # build pseudo_coords
         pseudo_coords = torch.zeros((self.B * (self.window_size - 1), n_points, 2),
                                     device=self.gpuid) # B*(window - 1) x N x 2
-        tgt_ids = torch.zeros(self.B * (self.window_size - 1), dtype=torch.int64)    # B*(window - 1)
-        src_ids = torch.zeros(self.B * (self.window_size - 1), dtype=torch.int64)    # B*(window - 1)
+        tgt_ids = torch.zeros(self.B * (self.window_size - 1), dtype=torch.int64, device=self.gpuid)    # B*(window - 1)
+        src_ids = torch.zeros(self.B * (self.window_size - 1), dtype=torch.int64, device=self.gpuid)    # B*(window - 1)
         # loop for each batch
         for i in range(self.B):
-            win_ids = torch.arange(i * self.window_size + 1, i * self.window_size + self.window_size)
+            win_ids = torch.arange(i * self.window_size + 1, i * self.window_size + self.window_size).to(self.gpuid)
             tgt_desc = keypoint_desc[win_ids]  # (window - 1) x C x N
             tgt_desc = F.normalize(tgt_desc, dim=1)
             match_vals = torch.matmul(tgt_desc.transpose(2, 1), src_desc_unrolled[i:i+1])  # (window - 1) x N x HW

networks/steam_solver.py
@@ -29,6 +29,8 @@ class SteamSolver():
                                 (self.batch_size, self.window_size - 1, 12, 1, 1))  # B x (W-1) x 12 x 4 x 4
         # steam solver (c++)
         self.solver_cpp = steamcpp.SteamSolver(config['steam']['time_step'], self.window_size)
+        if config['steam']['use_ransac']:
+            self.solver_cpp.useRansac()
         self.sigmapoints_flag = (config['steam']['expect_approx_opt'] == 1)
 
     def optimize(self, keypoint_coords, pseudo_coords, match_weights, keypoint_ints, time_tgt, time_src):
@@ -87,7 +89,7 @@ class SteamSolver():
                 times1 += [time_src[w].cpu().numpy().squeeze()]
                 times2 += [time_tgt[w].cpu().numpy().squeeze()]
             # solver
-            timestamps1, timestamps2 = getApproxTimeStamps(points1, points2, times1, times2)
+            timestamps1, timestamps2 = self.getApproxTimeStamps(points1, points2, times1, times2)
             self.solver_cpp.setMeas(points2, points1, weights, timestamps2, timestamps1)
             self.solver_cpp.optimize()
             # get pose output
@@ -102,7 +104,7 @@ class SteamSolver():
 
         return torch.from_numpy(R_tgt_src).to(self.gpuid), torch.from_numpy(t_src_tgt_in_tgt).to(self.gpuid)
 
-    def getApproxTimeStamps(points1, points2, times1, times2):
+    def getApproxTimeStamps(self, points1, points2, times1, times2):
         azimuth_step = (2 * np.pi) / 400
         # Helper to ensure angle is within [0, 2 * PI)
         def wrapto2pi(phi):
@@ -131,8 +133,9 @@ class SteamSolver():
                     phi = np.arctan2(y, x)
                     phi = wrapto2pi(phi)
                     time_idx = phi / azimuth_step
-                    t1 = times[int(np.floor(time_idx)), 0]
-                    t2 = times[int(np.ceil(time_idx)), 0]
+                    t1 = times[int(np.floor(time_idx))]
+                    idx2 = int(np.ceil(time_idx))
+                    t2 = times[idx2 if idx2 < 400 else 399]
                     # interpolate to get slightly more precise timestamp
                     ratio = time_idx % 1
                     t = int(t1 + ratio * (t2 - t1))