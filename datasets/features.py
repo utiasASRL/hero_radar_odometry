431f31858d242be5a67d2c70ddfceb0b1eac31e0
keenan-burnett <keenburn2004@gmail.com>
2021-04-21 19:45:30 +0000

    Ported over my hand-crafted feature detection and matching using ORB descriptors from c++ to python to see how well steam with motion compensation would do.

---
+++ datasets/features.py
@@ -0,0 +1,127 @@
+import numpy as np
+import cv2
+from scipy import ndimage
+
+def cen2018features(fft_data: np.ndarray, min_range=58, zq=4.0, sigma_gauss=17,
+                    max_range=1920, downsample=1) -> np.ndarray:
+    """Extract features from polar radar data using the method described in cen_icra18
+    Args:
+        fft_data (np.ndarray): Polar radar power readings
+        min_range (int): targets with a range bin less than or equal to this value will be ignored.
+        zq (float): if y[i] > zq * sigma_q then it is considered a potential target point
+        sigma_gauss (int): std dev of the gaussian filter used to smooth the radar signal
+        
+    Returns:
+        np.ndarray: N x 2 array of feature locations (azimuth_bin, range_bin)
+    """
+    nazimuths = fft_data.shape[0]
+    fft_data = fft_data[:, :max_range]
+    fft_data = fft_data[:, ::downsample]
+    sigma_gauss = int(sigma_gauss / downsample)
+    if sigma_gauss % 2 == 0:
+        sigma_gauss += 1
+    # w_median = 200
+    # q = fft_data - ndimage.median_filter(fft_data, size=(1, w_median))  # N x R
+    q = fft_data - np.mean(fft_data, axis=1, keepdims=True)
+    p = ndimage.gaussian_filter1d(q, sigma=sigma_gauss, truncate=3.0) # N x R
+    noise = np.where(q < 0, q, 0) # N x R
+    nonzero = np.sum(q < 0, axis=-1, keepdims=True) # N x 1
+    sigma_q = np.sqrt(np.sum(noise**2, axis=-1, keepdims=True) / nonzero) # N x 1
+
+    def norm(x, sigma):
+        return np.exp(-0.5 * (x / sigma)**2) / (sigma * np.sqrt(2 * np.pi))
+
+    nqp = norm(q - p, sigma_q)
+    npp = norm(p, sigma_q)
+    nzero = norm(np.zeros((nazimuths, 1)), sigma_q)
+    y = q * (1 - nqp / nzero) + p * ((nqp - npp) / nzero)
+    t = np.nonzero(y > zq * sigma_q)
+    # Extract peak centers
+    current_azimuth = t[0][0]
+    peak_points = [t[1][0]]
+    peak_centers = []
+
+    def mid_point(l):
+        return l[len(l) // 2]
+
+    for i in range(1, len(t[0])):
+        if t[1][i] - peak_points[-1] > 1 or t[0][i] != current_azimuth:
+            m = mid_point(peak_points)
+            if m > min_range:
+                peak_centers.append((current_azimuth, m))
+            peak_points = []
+        current_azimuth = t[0][i]
+        peak_points.append(t[1][i])
+    if len(peak_points) > 0 and mid_point(peak_points) > min_range:
+        peak_centers.append((current_azimuth, mid_point(peak_points)))
+
+    targets = np.asarray(peak_centers)
+    targets[:, 1] *= downsample
+    return np.asarray(peak_centers)
+
+def polar_to_cartesian_points(azimuths: np.ndarray, polar_points: np.ndarray, radar_resolution: float,
+    downsample_rate=1) -> np.ndarray:
+    """Converts points from polar coordinates to cartesian coordinates
+    Args:
+        azimuths (np.ndarray): The actual azimuth of reach row in the fft data reported by the Navtech sensor
+        polar_points (np.ndarray): N x 2 array of points (azimuth_bin, range_bin)
+        radar_resolution (float): Resolution of the polar radar data (metres per pixel)
+        downsample_rate (float): fft data may be downsampled along the range dimensions to speed up computation
+    Returns:
+        np.ndarray: N x 2 array of points (x, y) in metric
+    """
+    N = polar_points.shape[0]
+    cart_points = np.zeros((N, 2))
+    for i in range(0, N):
+        azimuth = azimuths[int(polar_points[i, 0])]
+        r = polar_points[i, 1] * radar_resolution * downsample_rate + radar_resolution / 2
+        cart_points[i, 0] = r * np.cos(azimuth)
+        cart_points[i, 1] = r * np.sin(azimuth)
+    return cart_points
+
+def convert_to_bev(cart_points: np.ndarray, cart_resolution: float, cart_pixel_width: int) -> np.ndarray:
+    """Converts points from metric cartesian coordinates to pixel coordinates in the BEV image
+    Args:
+        cart_points (np.ndarray): N x 2 array of points (x, y) in metric
+        cart_pixel_width (int): width and height of the output BEV image
+    Returns:
+        np.ndarray: N x 2 array of points (u, v) in pixels which can be plotted on the BEV image
+    """
+    if (cart_pixel_width % 2) == 0:
+        cart_min_range = (cart_pixel_width / 2 - 0.5) * cart_resolution
+    else:
+        cart_min_range = cart_pixel_width // 2 * cart_resolution
+    pixels = []
+    N = cart_points.shape[0]
+    for i in range(0, N):
+        u = (cart_min_range + cart_points[i, 1]) / cart_resolution
+        v = (cart_min_range - cart_points[i, 0]) / cart_resolution
+        if 0 < u and u < cart_pixel_width and 0 < v and v < cart_pixel_width:
+            pixels.append((u, v))
+    return np.asarray(pixels)
+
+def convert_to_bev2(cart_points, cart_resolution=0.2592, cart_pixel_width=640,
+                    patch_size=21):
+    if (cart_pixel_width % 2) == 0:
+        cart_min_range = (cart_pixel_width / 2 - 0.5) * cart_resolution
+    else:
+        cart_min_range = cart_pixel_width // 2 * cart_resolution
+    pixels = []
+    targets = []
+    N = cart_points.shape[0]
+    for i in range(N):
+        u = (cart_min_range + cart_points[i, 1]) / cart_resolution
+        v = (cart_min_range - cart_points[i, 0]) / cart_resolution
+        if 0 < u - patch_size and u + patch_size + 1 < cart_pixel_width and \
+            0 < v - patch_size and v + patch_size + 1 < cart_pixel_width:
+            pixels.append((u, v))
+            targets.append((cart_points[i, 0], cart_points[i, 1]))
+    return np.asarray(targets), np.asarray(pixels)
+
+def convert_to_keypoints(targets, patch_size=21):
+    N = targets.shape[0]
+    kp = []
+    for i in range(N):
+        kp.append(cv2.KeyPoint(targets[i, 0], targets[i, 1], patch_size))
+    return kp
+
--- networks/hero.py
+++ networks/hero.py
@@ -8,6 +8,9 @@ from networks.softmax_ref_matcher import SoftmaxRefMatcher
 from networks.steam_solver import SteamSolver
 from utils.utils import convert_to_radar_frame, mask_intensity_filter
 
+import cv2
+from datasets.features import *
+
 class HERO(torch.nn.Module):
     """
         This model performs unsupervised radar odometry using a sliding window optimization with a window
@@ -25,19 +28,61 @@ class HERO(torch.nn.Module):
         self.solver = SteamSolver(config)
         self.patch_size = config['networks']['keypoint_block']['patch_size']
         self.patch_mean_thres = config['steam']['patch_mean_thres']
+        self.orb = cv2.ORB_create()
+        self.orb.setPatchSize(21)
+        self.orb.setEdgeThreshold(21)
+        self.matcher = cv2.BFMatcher(cv2.NORM_HAMMING) #, crossCheck=True)
 
     def forward(self, batch):
         data = batch['data'].to(self.gpuid)
         mask = batch['mask'].to(self.gpuid)
         timestamps = batch['timestamps'].to(self.gpuid)
 
-        detector_scores, weight_scores, desc = self.unet(data)
+        '''detector_scores, weight_scores, desc = self.unet(data)
         keypoint_coords, keypoint_scores, keypoint_desc = self.keypoint(detector_scores, weight_scores, desc)
         pseudo_coords, match_weights, tgt_ids, src_ids = self.softmax_matcher(keypoint_scores, keypoint_desc, desc)
         keypoint_coords = keypoint_coords[tgt_ids]
 
         pseudo_coords_xy = convert_to_radar_frame(pseudo_coords, self.config)
-        keypoint_coords_xy = convert_to_radar_frame(keypoint_coords, self.config)
+        keypoint_coords_xy = convert_to_radar_frame(keypoint_coords, self.config)'''
+
+        tgt_ids = [1]
+        src_ids = [0]
+        polar = batch['polar'].cpu().numpy().squeeze()
+        azimuths = batch['azimuths'].cpu().numpy().squeeze()
+        ptgt1 = cen2018features(polar[0])
+        ptgt2 = cen2018features(polar[1])
+        tgt1 = polar_to_cartesian_points(azimuths[0], ptgt1, 0.0432)
+        tgt2 = polar_to_cartesian_points(azimuths[1], ptgt2, 0.0432)
+        tgt1, tgt1p = convert_to_bev2(tgt1, 0.2592, 640, 21)
+        tgt2, tgt2p = convert_to_bev2(tgt2, 0.2592, 640, 21)
+
+        kp1 = convert_to_keypoints(tgt1p)
+        kp2 = convert_to_keypoints(tgt2p)
+        img1 = (data[0] * 255).cpu().numpy().squeeze().astype(np.uint8)
+        img2 = (data[1] * 255).cpu().numpy().squeeze().astype(np.uint8)
+        kp1_, des1 = self.orb.compute(img1, kp1)
+        kp2_, des2 = self.orb.compute(img2, kp2)
+        assert(len(kp1_) == len(kp1))
+
+        matches = self.matcher.knnMatch(des1, des2, k=2)
+        good_matches = []
+        for i,(m,n) in enumerate(matches):
+            if m.distance < 0.7 * n.distance:
+                good_matches.append(m)
+        N = len(good_matches)
+        print(N)
+        p1 = np.zeros((N, 2))
+        p2 = np.zeros((N, 2))
+        for i,match in enumerate(good_matches):
+            p1[i, :] = tgt1[match.queryIdx, :]
+            p2[i, :] = tgt2[match.trainIdx, :]
+
+        p1 = np.expand_dims(p1, axis=0)
+        p2 = np.expand_dims(p2, axis=0)
+        pseudo_coords_xy = torch.from_numpy(p1)
+        keypoint_coords_xy = torch.from_numpy(p2)
+
         # rotate back if augmented
         if 'T_aug' in batch:
             T_aug = torch.stack(batch['T_aug'], dim=0).to(self.gpuid)
@@ -49,12 +94,20 @@ class HERO(torch.nn.Module):
             keypoint_coords_xy[:, :, 1] *= -1.0
 
         # binary mask to remove keypoints from 'empty' regions of the input radar scan
-        keypoint_ints = mask_intensity_filter(mask[tgt_ids], self.patch_size, self.patch_mean_thres)
-
-        time_tgt = torch.index_select(timestamps, 0, tgt_ids)
-        time_src = torch.index_select(timestamps, 0, src_ids)
+        #keypoint_ints = mask_intensity_filter(mask[tgt_ids], self.patch_size, self.patch_mean_thres)
+        time_tgt = timestamps[tgt_ids]
+        time_src = timestamps[src_ids]
+        keypoint_ints = torch.ones((1, 1, p1.shape[1]))
+        match_weights = torch.ones((1, 1, p1.shape[1]))
+        #time_tgt = torch.index_select(timestamps, 0, tgt_ids)
+        #time_src = torch.index_select(timestamps, 0, src_ids)
         R_tgt_src_pred, t_tgt_src_pred = self.solver.optimize(keypoint_coords_xy, pseudo_coords_xy, match_weights,
                                                               keypoint_ints, time_tgt, time_src)
+
+        weight_scores = None
+        detector_scores = None
+        keypoint_coords = None
+        pseudo_coords = None
 
         return {'R': R_tgt_src_pred, 't': t_tgt_src_pred, 'scores': weight_scores, 'tgt': keypoint_coords_xy,
                 'src': pseudo_coords_xy, 'match_weights': match_weights, 'keypoint_ints': keypoint_ints,
