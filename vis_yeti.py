cf3b65b27058bfefdcf66a19bc140ebf0ad084a0
keenan-burnett <keenburn2004@gmail.com>
2021-05-17 18:09:52 -0400

    whitespace

--- vis_yeti.py
+++ vis_yeti.py
@@ -26,7 +26,7 @@ def draw_match(batch, out, config, solver, inliers):
     keypoint_ints = out['keypoint_ints']
     ids = torch.nonzero(keypoint_ints[0, 0] > 0, as_tuple=False).squeeze(1)
     ids_cpu = ids.cpu()
-	src = out['src'][0, ids].squeeze().detach().cpu().numpy()
+    src = out['src'][0, ids].squeeze().detach().cpu().numpy()
     tgt = out['tgt'][0, ids].squeeze().detach().cpu().numpy()
     inliers = inliers[0]
     src = src[inliers]
@@ -48,17 +48,17 @@ def draw_match(batch, out, config, solver, inliers):
     # plt.savefig('match1.png', bbox_inches='tight', pad_inches=0.0)
 
 def get_closest_image(folder, ref_time):
-	files = os.listdir(folder)
-	min_delta = 1e6
-	closest = -1
-	for i, file in enumerate(files):
-		time = int(file.split('.')[0])
-		delta = abs(time - ref_time)
-		if delta < min_delta:
-			min_delta = delta
-			closest = i
-	assert(closest != -1)
-	frame = cv2.imread(folder + files[closest])
+    files = os.listdir(folder)
+    min_delta = 1e6
+    closest = -1
+    for i, file in enumerate(files):
+        time = int(file.split('.')[0])
+        delta = abs(time - ref_time)
+        if delta < min_delta:
+            min_delta = delta
+            closest = i
+    assert(closest != -1)
+    frame = cv2.imread(folder + files[closest])
     H = 720
     W = 1280
     upper_crop = 240
@@ -68,16 +68,16 @@ def get_closest_image(folder, ref_time):
     return cv2.resize(frame, (W, H))
 
 def panel_and_save(match_img, cam_img, odom_img, save_path):
-	match_img = cv2.resize(match_img, (640, 640))
-	odom_img = cv2.resize(odom_img, (640, 640))
-	comb = cv2.hconcat([match_img, odom_img])
-	comb = cv2.vconcat([cam_img, comb])
-	cv2.imsave(save_path)
+    match_img = cv2.resize(match_img, (640, 640))
+    odom_img = cv2.resize(odom_img, (640, 640))
+    comb = cv2.hconcat([match_img, odom_img])
+    comb = cv2.vconcat([cam_img, comb])
+    cv2.imsave(save_path)
 
 if __name__ == '__main__':
-	with open('config/steam.json') as f:
-		config = json.load(f)
-	root = './'
+    with open('config/steam.json') as f:
+        config = json.load(f)
+    root = './'
 
     if config['model'] == 'UnderTheRadar':
         model = UnderTheRadar(config).to(config['gpuid'])
@@ -85,22 +85,22 @@ if __name__ == '__main__':
         model = HERO(config).to(config['gpuid'])
         model.solver.sliding_flag = True
     elif config['model'] == 'YETI':
-    	model = YETI(config)
-    	model.solver.sliding_flag = True
+        model = YETI(config)
+        model.solver.sliding_flag = True
 
     if config['model'] == 'UnderTheRadar' or config['model'] == 'HERO':
-    	assert(args.pretrain is not None)
-    	checkpoint = torch.load(args.pretrain, map_location=torch.device(config['gpuid']))
-    	    failed = False
-	    try:
-	        model.load_state_dict(checkpoint['model_state_dict'], strict=False)
-	    except Exception as e:
-	        print(e)
-	        failed = True
-	    if failed:
-	        model.load_state_dict(checkpoint, strict=False)
+        assert(args.pretrain is not None)
+        checkpoint = torch.load(args.pretrain, map_location=torch.device(config['gpuid']))
+            failed = False
+        try:
+            model.load_state_dict(checkpoint['model_state_dict'], strict=False)
+        except Exception as e:
+            print(e)
+            failed = True
+        if failed:
+            model.load_state_dict(checkpoint, strict=False)
 
-	model.eval()
+    model.eval()
 
     if config['dataset'] == 'oxford':
         _, _, test_loader = get_dataloaders(config)
@@ -109,35 +109,35 @@ if __name__ == '__main__':
 
     seq_name = test_loader.dataset.sequences[0]
 
-	T_gt = []
-	T_pred = []
+    T_gt = []
+    T_pred = []
 
     for batchi, batch in enumerate(test_loader):
-    	with torch.no_grad():
-    		out = model(batch)
-    	T_gt.append(batch['T_21'][w].numpy().squeeze())
-    	T_pred.append(get_T_ba(out, a=0, b=1))
-    	inliers = []
-    	model.solver.solver_cpp.getInliers(inliers)
-    	match_img = draw_match(batch, out, config, solver, inliers)
-    	print(match_img.shape)
-    	cv2.imsave('match.png', match_img)
+        with torch.no_grad():
+            out = model(batch)
+        T_gt.append(batch['T_21'][w].numpy().squeeze())
+        T_pred.append(get_T_ba(out, a=0, b=1))
+        inliers = []
+        model.solver.solver_cpp.getInliers(inliers)
+        match_img = draw_match(batch, out, config, solver, inliers)
+        print(match_img.shape)
+        cv2.imsave('match.png', match_img)
 
-    	# Get closest camera image, crop it
-    	radar_time = batch['t_ref'][0][0].item()
-    	cam_img = get_closest_image(radar_time, config['data_dir'] + seq_name + '/camera/')
-    	print(cam_img.shape)
-    	cv2.imsave('cam.png', cam_img)
+        # Get closest camera image, crop it
+        radar_time = batch['t_ref'][0][0].item()
+        cam_img = get_closest_image(radar_time, config['data_dir'] + seq_name + '/camera/')
+        print(cam_img.shape)
+        cv2.imsave('cam.png', cam_img)
 
 
-    	# draw odom path 
-    	odom_img = plot_sequences(T_gt, T_pred, [len(T_gt)], returnTensor=False)
-    	print(odom_img.shape)
-    	cv2.imsave('odom.png', odom_img)
+        # draw odom path 
+        odom_img = plot_sequences(T_gt, T_pred, [len(T_gt)], returnTensor=False)
+        print(odom_img.shape)
+        cv2.imsave('odom.png', odom_img)
 
-    	# panel the images together, save as png file
-    	panel_and_save(match_img, cam_img, odom_img, root + str(batchi) + '.png')
+        # panel the images together, save as png file
+        panel_and_save(match_img, cam_img, odom_img, root + str(batchi) + '.png')
 
-    	break
+        break
 
 
