83625b35e405d6918169846833cd9b75fb2e0fb5
Keenan Burnett <keenburn2004@gmail.com>
2021-06-27 20:43:43 -0400

    Update download_models.sh

--- download_models.sh
+++ download_models.sh
@@ -2,5 +2,6 @@
 wget -c https://drive.google.com/uc?id=1ubec-20hOCOOXJgFspuQXHfSH7AjPNcw -O models.zip
 unzip models.zip
 mkdir -p models
-mv hero* models
+mv *.pt models
+mv *.json models
 rm models.zip
